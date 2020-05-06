a81dfc14b96f9403c12069f7fecf982a837f1053
godrid5 <64898617+godrid5@users.noreply.github.com>
2020-05-06 12:34:32 +0200

    Update EjercicioGitNetbeansGodEsc.java

--- src/ejerciciogitnetbeansgodesc/EjercicioGitNetbeansGodEsc.java
+++ src/ejerciciogitnetbeansgodesc/EjercicioGitNetbeansGodEsc.java
@@ -11,6 +11,9 @@ public class EjercicioGitNetbeansGodEsc {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
+        System.out.println("hola mundo desde github");
+        
       
     }
     
